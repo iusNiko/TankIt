4e5bd8382163cbdf4ce82974efbc6b45374a1e94
Michał Nikonowicz <mniko.nikonowicz@gmail.com>
2025-02-28 21:35:39 +0100

    Added tank model, Acceleration, etc.

---
+++ Code/PlayerCamera.cs
@@ -0,0 +1,5 @@
+using Sandbox;
+
+public partial class PlayerCamera : Component {
+    
+}
--- Code/PlayerTank.cs
+++ Code/PlayerTank.cs
@@ -2,16 +2,20 @@ using Sandbox;
 
 public partial class PlayerTank : Component
 {
-    [Property] public float Speed = 30;
+    [Property] public float MaxSpeed = 500;
+    [Property] public float Acceleration = 200;
+    [Property] public float Deceleration = 400;
     [Property] public float RotationSpeed = 20;
     [Property] public GameObject Barrel;
     [Property] public GameObject Bullet;
     [Property] public float Cooldown = 0.5f;
+    public float CurrentSpeed = 0;
 
     private float timeSinceLastShot;
 
      protected override void OnStart()
     {
+        Mouse.Visible = true;
         timeSinceLastShot = Cooldown;
     }
 
@@ -28,9 +32,8 @@ public partial class PlayerTank : Component
         if(Input.Pressed("attack1") && timeSinceLastShot >= Cooldown)
         {
             GameObject bullet = Bullet.Clone(Barrel.WorldPosition);
-            bullet.LocalRotation = LocalRotation;
+            bullet.WorldRotation = Rotation.From(Barrel.WorldRotation.Pitch(), Barrel.WorldRotation.Yaw() + 90, Barrel.WorldRotation.Roll());
             timeSinceLastShot = 0;
-
         }
     }
     
@@ -38,13 +41,43 @@ public partial class PlayerTank : Component
     {
         if(Input.Down("Forward")) 
         {
-            LocalPosition += LocalRotation.Forward * Speed * Time.Delta;
+            if(CurrentSpeed > 0) {
+                CurrentSpeed += Acceleration * Time.Delta;
+            }
+            else {
+                CurrentSpeed += Deceleration * Time.Delta;
+            }
+            
+        }
+        else if(Input.Down("Backward"))
+        {
+            if(CurrentSpeed < 0) {
+                CurrentSpeed -= Acceleration * Time.Delta;
+            }
+            else {
+                CurrentSpeed -= Deceleration * Time.Delta;
+            }
+        }
+        else {
+            if(CurrentSpeed > 0) {
+                CurrentSpeed -= Acceleration * Time.Delta;
+                if(CurrentSpeed < 0) {
+                    CurrentSpeed = 0;
+                }
+            }
+            else if(CurrentSpeed < 0) {
+                CurrentSpeed += Acceleration * Time.Delta;
+                if(CurrentSpeed > 0) {
+                    CurrentSpeed = 0;
+                }
+            }
         }
 
-        if(Input.Down("Backward"))
-        {
-            LocalPosition += LocalRotation.Backward * Speed * Time.Delta;
+        if(CurrentSpeed > MaxSpeed) {
+            CurrentSpeed = MaxSpeed;
         }
+
+        LocalPosition += LocalRotation.Left * CurrentSpeed * Time.Delta;
 
         if(Input.Down("Left"))
         {
